930a985cdfad0335bf1bc4d4d69e44e9353f3187
foxy773 <felix_1999@live.no>
2022-05-02 14:48:51 +0200

    :lipstick: Add small UI changes to the navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -10,7 +10,7 @@
           <li class="list__item">
               <button class="item__button">
                   <p class="button__text">Logg inn</p>
-                  <img class="button__img" src="/images/right-arrow.png" alt="">
+                  <!-- <img class="button__img" src="/images/right-arrow.png" alt=""> -->
             </button>
           </li>
       </ul>
@@ -52,20 +52,29 @@ export default {
         background: var(--highlight);
         border-radius: 0.4rem;
         padding: 1rem;
+        cursor: pointer;
     }
 
     .item__button:hover {
-        transform: translate(1, 1) rotate(5) skewX(1) skewY(1) scaleX(1) scaleY(1);
+        -webkit-transform: scale(1.03);
+        box-shadow: 0px 4px 7px 0px rgba(0,0,0,0.75);
     }
 
     .button__text {
-        
+        font-family: var(--font-family);
+        font-weight: 500;
+        font-size: .875rem;
     }
 
     .button__img {
         height: 100%;
         width: auto;
+        padding: 0.1rem 0.1rem;
     }
+
+    /* @-webkit-keyframes {
+        
+    } */
 
     
 
