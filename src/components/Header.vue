9ecdb3c381ed968cdec61a27fa363fca95ba0d0b
foxy773 <felix_1999@live.no>
2022-05-02 13:57:46 +0200

    :tada: Start new project

---
+++ src/components/Header.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="header">
+      <div class="header__content">
+            <img class="content__logo" src="/images/Norsk-tipping-logo-trans2.png" alt="">
+            <Navbar />
+      </div>
+      
+  </div>
+</template>
+
+<script>
+import Navbar from "./Navbar.vue"
+
+export default {
+    components: {
+        Navbar
+    }
+}
+</script>
+
+<style scoped>
+    .header {
+        width: 100%;
+        height: 6rem;
+        max-height: 6rem;
+        background: var(--background2);
+        padding: 0.2rem;
+        display: flex;
+        
+    }
+
+    .header__content {
+        width: 100%;
+        height: 100%;
+        padding: 0 10rem;
+        display: flex;
+        justify-content: space-between;
+    }
+
+    .content__logo {
+        height: 100%;
+        width: auto;
+    }
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,72 @@
+<template>
+  <div class="navBar">
+      <ul class="navBar__list">
+          <li class="list__item">
+              <button class="item__button">
+                  <img src="" alt="">
+                  <p class="button__text">Registrer deg her</p>
+            </button>
+          </li>
+          <li class="list__item">
+              <button class="item__button">
+                  <p class="button__text">Logg inn</p>
+                  <img class="button__img" src="/images/right-arrow.png" alt="">
+            </button>
+          </li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    .navBar{
+        width: 16rem;
+        height: 100%;
+        background: inherit;
+        display: flex;
+    }
+
+    .navBar__list {
+        width: 100%;
+        height: 100%;
+        display: flex;
+        list-style: none;
+        justify-content: space-between;
+    }
+
+    .list__item {
+        display: flex;
+        align-items: center;
+    }
+
+    .item__button {
+        display: flex;
+        align-items: center;
+        height: 3rem;
+        border: 0;
+        background: var(--highlight);
+        border-radius: 0.4rem;
+        padding: 1rem;
+    }
+
+    .item__button:hover {
+        transform: translate(1, 1) rotate(5) skewX(1) skewY(1) scaleX(1) scaleY(1);
+    }
+
+    .button__text {
+        
+    }
+
+    .button__img {
+        height: 100%;
+        width: auto;
+    }
+
+    
+
+</style>
